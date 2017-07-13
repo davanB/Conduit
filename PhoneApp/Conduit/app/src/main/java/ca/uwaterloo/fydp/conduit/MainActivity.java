2ba98737dd0bc73356be1a960eb241dae2055f8c
davanB <davan.basran@gmail.com>
2017-07-13 00:25:10 -0400

    feature/expandable-floating-button, got basic button working

--- PhoneApp/Conduit/app/src/main/java/ca/uwaterloo/fydp/conduit/MainActivity.java
+++ PhoneApp/Conduit/app/src/main/java/ca/uwaterloo/fydp/conduit/MainActivity.java
@@ -1,21 +1,20 @@
 package ca.uwaterloo.fydp.conduit;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionMenu;
+import com.github.clans.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionMenu mainMenu = (FloatingActionMenu) findViewById(R.id.menu_red);
+        mainMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -33,9 +32,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        final FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        final FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        final FloatingActionButton fab3 = (FloatingActionButton) findViewById(R.id.fab3);
+
+        fab1.setLabelText("Button 1");
+        fab2.setLabelText("Button 2");
+        fab3.setLabelText("Button 3");
+
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.fab1:
+                        Toast toast = Toast.makeText(MainActivity.this,"fab1 pressed", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        break;
+                    case R.id.fab2:
+                        Toast toast2 = Toast.makeText(MainActivity.this,"fab2 pressed", Toast.LENGTH_LONG);
+                        toast2.setGravity(Gravity.CENTER, 0, 0);
+                        toast2.show();
+                        break;
+                    case R.id.fab3:
+                        Toast toast3 = Toast.makeText(MainActivity.this,"fab3 pressed", Toast.LENGTH_LONG);
+                        toast3.setGravity(Gravity.CENTER, 0, 0);
+                        toast3.show();
+                        break;
+                }
+            }
+        };
+
+        fab1.setOnClickListener(clickListener);
+        fab2.setOnClickListener(clickListener);
+        fab3.setOnClickListener(clickListener);
     }
 
     @Override
@@ -65,4 +96,9 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
 }
