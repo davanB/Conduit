cccbe936d0742afd5f28def4018a75429a38bd6d
Aaron Lobo <lobo.aaron1@gmail.com>
2017-07-18 13:16:36 -0400

    Refactor DesktopClient app to use gradle

--- DesktopClient/src/com/conduit/desktop/Main.java
+++ DesktopClient/src/com/conduit/desktop/Main.java
@@ -15,8 +15,9 @@ public class Main {
         while(true) {
             Scanner in = new Scanner(System.in);
             int num = in.nextInt();
-            dataLink.write("Hello world.".getBytes());
-//            dataLink.write("Bacon ipsum dolor amet ball tip capicola flank pork chop, andouille chuck bacon ham drumstick jerky cupim prosciutto filet mignon sirloin ham hock. Pork loin burgdoggen jowl alcatra meatloaf spare ribs capicola sausage rump. Ham hock sirloin turducken meatloaf corned beef shank. Shoulder doner spare ribs jowl turkey meatloaf frankfurter bresaola cow capicola short loin shankle ground round.".getBytes());
+//            dataLink.write("Hello world.".getBytes());
+            dataLink.write("0Bacon ipsum dolor amet ball tad1Bacon ipsum dolor amet ball tad2Bacon ipsum dolor amet ball tad3Bacon ipsum dolor amet ball tad4Bacon ipsum dolor amet ball tad5Bacon ipsum dolor amet ball tad6Bacon ipsum dolor amet ball tad7Bacon ipsum dolor amet ball tad8Bacon ipsum dolor amet ball tad9Bacon ipsum dolor amet ball tadABacon ipsum dolor amet ball tadBBacon ipsum dolor amet ball tadCBacon ipsum dolor amet ball tadDBacon ipsum dolor amet ball tad".getBytes());
+
 //            dataLink.openReadingPipe((byte)1, (byte)'b');
         }
     }
