55b3182c8096a004dcaa12ff654a160311c0c51b
Aaron Lobo <lobo.aaron1@gmail.com>
2018-03-08 13:06:54 -0500

    Update DesktopClient to use Kotlin

--- DesktopClient/src/com/conduit/desktop/Main.java
+++ DesktopClient/src/com/conduit/desktop/Main.java
@@ -1,8 +1,15 @@
 package com.conduit.desktop;
 
+import com.conduit.libdatalink.ConduitGroup;
 import com.conduit.libdatalink.DataLink;
 import com.conduit.libdatalink.DataLinkListener;
+import com.conduit.libdatalink.conduitabledata.ConduitConnectionEvent;
+import com.conduit.libdatalink.conduitabledata.ConduitMessage;
+import com.conduit.libdatalink.conduitabledata.ConduitableData;
+import com.conduit.libdatalink.conduitabledata.ConduitableDataTypes;
 import com.fazecast.jSerialComm.SerialPort;
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 import javax.xml.crypto.Data;
 import java.io.File;
@@ -23,6 +30,8 @@ public class Main {
     private static final int ACTION_SEND_IMAGE = 2;
     private static final int ACTION_SERIAL_THROUGHPUT = 3;
     private static final int ACTION_NETWORK_THROUGHPUT = 4;
+    private static final int ACTION_CONDUIT_CONNECTION_EVENT = 5;
+    private static final int ACTION_CONDUIT_GROUP_FLOW = 6;
     private static final int ACTION_STATS = 9;
 
     private static final byte PACKET_TYPE_DEBUG_JUNK = 1;
@@ -52,30 +61,30 @@ public class Main {
         if (portNumber == -1) portNumber = in.nextInt();
 
         dataLink = new DataLink(new UsbDriver(serialPorts.get(portNumber)));
-        dataLink.addReadListener(new DataLinkListener() {
-            @Override
-            public void OnReceiveData(int originAddress, byte payloadType, ByteBuffer payload) {
-                byte[] buf = new byte[payload.remaining()];
-                payload.get(buf);
+//        dataLink.addReadListener(new DataLinkListener() {
+//            @Override
+//            public void OnReceiveData(int originAddress, byte payloadType, ByteBuffer payload) {
+//                byte[] buf = new byte[payload.remaining()];
+//                payload.get(buf);
+//
+//                switch (payloadType) {
+//                    case PACKET_TYPE_DEBUG_IMAGE:
+//                        File fi = new File("tmp/" + System.currentTimeMillis() + ".jpg");
+//                        fi.getParentFile().mkdirs();
+//                        try {
+//                            Files.write(fi.toPath(), buf);
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+//                        break;
+//                    default:
+//                        System.out.println(String.format("0x%08X %1s", originAddress, new String(buf)));
+//                        break;
+//                }
+//            }
+//        });
 
-                switch (payloadType) {
-                    case PACKET_TYPE_DEBUG_IMAGE:
-                        File fi = new File("tmp/" + System.currentTimeMillis() + ".jpg");
-                        fi.getParentFile().mkdirs();
-                        try {
-                            Files.write(fi.toPath(), buf);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        break;
-                    default:
-                        System.out.println(String.format("0x%08X %1s", originAddress, new String(buf)));
-                        break;
-                }
-            }
-        });
-
-        connect();
+//        connect();
 //        sendString();
 
         while(true) {
@@ -85,6 +94,8 @@ public class Main {
             System.out.println(String.format("[%d] Send Image", ACTION_SEND_IMAGE));
             System.out.println(String.format("[%d] Serial Throughput", ACTION_SERIAL_THROUGHPUT));
             System.out.println(String.format("[%d] Network Throughput", ACTION_NETWORK_THROUGHPUT));
+            System.out.println(String.format("[%d] CCE", ACTION_CONDUIT_CONNECTION_EVENT));
+            System.out.println(String.format("[%d] Conduit Group Flow", ACTION_CONDUIT_GROUP_FLOW));
             System.out.println(String.format("[%d] Dump Stats", ACTION_STATS));
 
             int action = in.nextInt();
@@ -106,6 +117,12 @@ public class Main {
                     networkThroughput();
                     dataLink.statsCollector.printStats();
                     break;
+                case ACTION_CONDUIT_CONNECTION_EVENT:
+                    conduitConnectionEvent();
+                    break;
+                case ACTION_CONDUIT_GROUP_FLOW:
+                    conduitGroupFlow();
+                    break;
                 case ACTION_STATS:
                     dataLink.statsCollector.printStats();
                     break;
@@ -116,21 +133,36 @@ public class Main {
 
     private static void connect() {
         // Only the last byte should differ
-        int addrA = 0xCDABCD71;
-        int addrB = 0xCDABCD69;
+        int addrA = 0xABCDEF01; // Me
+        int addrB = 0xABCDEF04;
+//        int addrA = 0xABCDEF27; // Me
+//        int addrB = 0xABCDEF13;
 
         // Assuming ports are off by one
         if (portNumber % 2 == 0) {
             System.out.println("This is radio A");
-            dataLink.openWritingPipe(addrA);
             dataLink.openReadingPipe((byte)1, addrB);
+            dataLink.openReadingPipe((byte)2, 0xABCDEF06);
+            dataLink.openReadingPipe((byte)3, 0xABCDEF07);
+            dataLink.openReadingPipe((byte)4, 0xABCDEF08);
+//            dataLink.openReadingPipe((byte)5, 0xABCDEF09);
+            dataLink.openWritingPipe(addrA);
             remote = addrB;
         } else {
             System.out.println("This is radio B");
-            dataLink.openWritingPipe(addrB);
             dataLink.openReadingPipe((byte)1, addrA);
+            dataLink.openReadingPipe((byte)2, 0xABCDEF06);
+            dataLink.openReadingPipe((byte)3, 0xABCDEF07);
+            dataLink.openReadingPipe((byte)4, 0xABCDEF08);
+//            dataLink.openReadingPipe((byte)5, 0xABCDEF09);
+            dataLink.openWritingPipe(addrB);
             remote = addrA;
         }
+//        System.out.println("This is radio A");
+//        dataLink.openWritingPipe(0xABCDEF0D);
+//        dataLink.openReadingPipe((byte)1, 0xABCDEF04);
+//        remote = addrA;
+
         System.out.println("Ready to Tx/Rx");
     }
 
@@ -165,6 +197,75 @@ public class Main {
         byte[] PAYLOAD = new byte[1024 * 2];
         new Random().nextBytes(PAYLOAD);
         dataLink.write((byte) 1, PAYLOAD);
+    }
+
+    static int clientId = 1;
+    private static void conduitConnectionEvent() {
+        System.out.println("Sending Conduit Connection Event");
+        ConduitConnectionEvent cce = new ConduitConnectionEvent(clientId, "Hello");
+        ByteBuffer buff = cce.getPayload();
+        dataLink.write((byte) 3, buff.array());
+        clientId++;
+    }
+
+    private static void conduitGroupFlow() {
+        int masterClientId = 0;
+        int slaveClientId = 1;
+
+        if (portNumber % 2 == 0) {
+
+            System.out.println("[Master] I am Master");
+
+            ConduitGroup conduitGroup = new ConduitGroup(dataLink, 0xABCDEF00, masterClientId);
+            conduitGroup.addConduitableDataListener(ConduitableDataTypes.CONNECTION_EVENT, new Function1<ConduitableData, Unit>() {
+                @Override
+                public Unit invoke(ConduitableData conduitableData) {
+
+                    System.out.println("[Master] Got client message");
+                    conduitGroup.send(slaveClientId, new ConduitMessage("Hello, got ur message!"));
+
+                    conduitGroup.addConduitableDataListener(ConduitableDataTypes.MESSAGE, new Function1<ConduitableData, Unit>() {
+                        @Override
+                        public Unit invoke(ConduitableData conduitableData) {
+
+                            System.out.println("[Master] Flow complete!");
+
+                            return null;
+                        }
+                    });
+
+                    return null;
+                }
+            });
+
+
+        } else {
+
+            System.out.println("[Client] My name Jeff");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            ConduitGroup conduitGroup = new ConduitGroup(dataLink, 0xABCDEF00, slaveClientId);
+            conduitGroup.addConduitableDataListener(ConduitableDataTypes.MESSAGE, new Function1<ConduitableData, Unit>() {
+                @Override
+                public Unit invoke(ConduitableData conduitableData) {
+
+                    System.out.println("[Client] Got Ledger");
+                    conduitGroup.send(masterClientId, new ConduitMessage("Got the Ledger!"));
+
+                    return null;
+                }
+            });
+
+            System.out.println("[Client] Sent Connection Event");
+            conduitGroup.send(masterClientId, new ConduitConnectionEvent(slaveClientId, "Aaron"));
+
+        }
+
     }
 
     private static List<String> getSerialPortList() {
