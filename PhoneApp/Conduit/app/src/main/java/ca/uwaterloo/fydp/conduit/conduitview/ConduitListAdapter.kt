fc3378634d65e235b9f28a1d437b96543b0a5575
Alvin Joys <alvin.joys@gmail.com>
2018-03-08 20:30:35 -0500

    Images on list view

--- PhoneApp/Conduit/app/src/main/java/ca/uwaterloo/fydp/conduit/conduitview/ConduitListAdapter.kt
+++ PhoneApp/Conduit/app/src/main/java/ca/uwaterloo/fydp/conduit/conduitview/ConduitListAdapter.kt
@@ -1,10 +1,13 @@
 package ca.uwaterloo.fydp.conduit.conduitview
 
+import android.media.Image
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import ca.uwaterloo.fydp.conduit.ConduitImage
 import ca.uwaterloo.fydp.conduit.R
 import ca.uwaterloo.fydp.conduit.connectionutils.ConduitManager
 import com.conduit.libdatalink.conduitabledata.ConduitGpsLocation
@@ -37,18 +40,29 @@ class ConduitListAdapter(private val data: List<ConduitableData>) : RecyclerView
             nameTextView.text = ConduitManager.getLedger().getUserNameForId(conduitableData.originAddress and 0x000000FF) + ": "
         }
     }
+    class ImageViewHolder(rootView: View) : BaseViewHolder(rootView){
+        val nameTextView = rootView.findViewById<TextView>(R.id.user_name)
+        val imageView = rootView.findViewById<ImageView>(R.id.image)
+        override fun bind(conduitableData: ConduitableData) {
+            nameTextView.text = ConduitManager.getLedger().getUserNameForId(conduitableData.originAddress and 0x000000FF) + ": "
+            imageView.setImageBitmap((conduitableData as ConduitImage).image)
+        }
+
+    }
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup,
                                     viewType: Int): ConduitListAdapter.BaseViewHolder {
         val layoutId = when (viewType) {
             ConduitableDataTypes.GPS_COORDS.flag.toInt() -> R.layout.conduit_list_gps_view
+            ConduitableDataTypes.IMAGE.flag.toInt() -> R.layout.conduit_list_image_view
             else -> R.layout.conduit_list_message_view
         }
         val rootView = LayoutInflater.from(parent.context)
                 .inflate(layoutId, parent, false)
         val viewHolder = when(viewType) {
             ConduitableDataTypes.GPS_COORDS.flag.toInt() -> GPSViewHolder(rootView)
+            ConduitableDataTypes.IMAGE.flag.toInt() -> ImageViewHolder(rootView)
             else -> MessageViewHolder(rootView)
         }
         return viewHolder
