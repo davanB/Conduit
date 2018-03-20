7e9d2d7bba419c4b8f5517b515e72ba380b151a6
Davan Basran <davan.basran@gmail.com>
2018-03-20 16:37:07 -0400

    class to encapsulate start/stop playing/recording audio

---
+++ PhoneApp/Conduit/app/src/main/java/ca/uwaterloo/fydp/conduit/conduitview/AudioRecord.java
@@ -0,0 +1,75 @@
+package ca.uwaterloo.fydp.conduit.conduitview;
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
+public class AudioRecord {
+    private static String mOutputFileName = null;
+    private MediaRecorder mRecorder = null;
+    private MediaPlayer mPlayer = null;
+
+    public void SetOutputFile(String fileName) {
+        mOutputFileName = fileName;
+    }
+
+    // start and stop recording audio
+    public void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    // start and stop playing audio
+    public void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mOutputFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            // error something went wrong
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioChannels(1);
+        mRecorder.setAudioSamplingRate(8000); // 8kHz
+        mRecorder.setAudioEncodingBitRate(12200);
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mRecorder.setOutputFile(mOutputFileName);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+}
